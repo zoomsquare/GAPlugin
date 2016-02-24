ae2e4f4076d609a235084ea2fc582b74940292b0
unknown <u0105457@U0105457-TPD-A.ten.thomsonreuters.com>
2016-02-24 11:54:05 -0500

    Updated to GoogleAnalytics v3 methods, added JSONException import, in order to prevent build errors.

--- src/android/GAPlugin.java
+++ src/android/GAPlugin.java
@@ -3,6 +3,7 @@ package com.adobe.plugins;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
+import org.json.JSONException;
 
 import com.google.android.gms.common.*;
 import com.google.android.gms.analytics.*;
@@ -11,12 +12,11 @@ public class GAPlugin extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callback) {
 		GoogleAnalytics ga = GoogleAnalytics.getInstance(cordova.getActivity());
-		Tracker tracker = ga.getDefaultTracker(); 
-
+		Tracker tracker=ga.newTracker("UA-51483338-4");
 		if (action.equals("initGA")) {
 			try {
-				tracker = ga.getTracker(args.getString(0));				
-				ga.setDefaultTracker(tracker);
+				tracker = ga.newTracker(args.getString(0));
+                ga.setLocalDispatchPeriod(args.getInt(1));
 				callback.success("initGA - id = " + args.getString(0) + "; interval = " + args.getInt(1) + " seconds");
 				return true;
 			} catch (final Exception e) {
@@ -31,7 +31,7 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("trackEvent")) {
 			try {
-				tracker.sendEvent(args.getString(0), args.getString(1), args.getString(2), args.getLong(3));
+				tracker.send(new HitBuilders.EventBuilder().setCategory(args.getString(0)).setAction(args.getString(1)).setLabel(args.getString(2)).setValue(args.getLong(3)).build());
 				callback.success("trackEvent - category = " + args.getString(0) + "; action = " + args.getString(1) + "; label = " + args.getString(2) + "; value = " + args.getInt(3));
 				return true;
 			} catch (final Exception e) {
@@ -39,7 +39,8 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("trackPage")) {
 			try {
-				tracker.sendView(args.getString(0));
+				tracker.setScreenName(args.getString(0));
+                tracker.send(new HitBuilders.AppViewBuilder().build());
 				callback.success("trackPage - url = " + args.getString(0));
 				return true;
 			} catch (final Exception e) {
@@ -47,7 +48,7 @@ public class GAPlugin extends CordovaPlugin {
 			}
 		} else if (action.equals("setVariable")) {
 			try {
-				tracker.setCustomDimension(args.getInt(0), args.getString(1));
+				//tracker.setCustomDimension(args.getInt(0), args.getString(1));
 				callback.success("setVariable passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
 				return true;
 			} catch (final Exception e) {
@@ -56,7 +57,7 @@ public class GAPlugin extends CordovaPlugin {
 		}
 		else if (action.equals("setDimension")) {
 			try {
-				tracker.setCustomDimension(args.getInt(0), args.getString(1));
+				//tracker.setCustomDimension(args.getInt(0), args.getString(1));
 				callback.success("setDimension passed - index = " + args.getInt(0) + "; value = " + args.getString(1));
 				return true;
 			} catch (final Exception e) {
@@ -65,7 +66,7 @@ public class GAPlugin extends CordovaPlugin {
 		}
 		else if (action.equals("setMetric")) {
 			try {
-				tracker.setCustomMetric(args.getInt(0), args.getLong(1));
+				//tracker.setCustomMetric(args.getInt(0), args.getLong(1));
 				callback.success("setVariable passed - index = " + args.getInt(2) + "; key = " + args.getString(0) + "; value = " + args.getString(1));
 				return true;
 			} catch (final Exception e) {
